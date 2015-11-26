d4a03840d2c188a664f07c0ff21de6754558168b
Mark St.Godard <markstgodard@gmail.com>
2015-11-26 14:07:28 -0600

    Fix messages loop

--- benchmark/benchmark_request.go
+++ benchmark/benchmark_request.go
@@ -65,24 +65,11 @@ func (br *BenchmarkRequest) Do() (BenchmarkResponse, error) {
 	return response, nil
 }
 
-func complete(messages []*events.Envelope) bool {
-	var http, log bool
-	for _, message := range messages {
-		switch *message.EventType {
-		case events.Envelope_HttpStartStop:
-			http = true
-		case events.Envelope_LogMessage:
-			log = true
-		}
-	}
-	return http && log
-}
-
 func (br *BenchmarkRequest) grabMessages() error {
 	messages := []*events.Envelope{}
 	timeout := time.After(br.timeout)
 
-	for !complete(messages) {
+	for len(messages) < 2 {
 		select {
 		case message := <-br.ch:
 			if br.checkMessage(message) {
